502ba43bebd37c2c77cf5ff5f0702851fed4838c
Janusz Strycharczyk <jachu85@gmail.com>
2018-01-20 14:18:10 +0100

    ver 1

---
+++ ThreadsExercises_1/ContanerOfInt.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadsExercises_1
+{
+    public sealed class ContainerOfInt
+    {
+        private static ContainerOfInt o_instance = null;
+        private List<int> o_listOfInts;
+        public bool isEmpty { get; private set; } = false;
+        private static Object singletonlock = new object();
+        public static ContainerOfInt Instance
+        {
+            get
+            {
+                lock (singletonlock)
+                {
+                    if (o_instance == null)
+                    {
+                        o_instance = new ContainerOfInt();
+                        o_instance.FillList();
+                    }
+
+                    return o_instance;
+                }                
+            }
+        }
+
+        private ContainerOfInt()
+        {
+            o_listOfInts = new List<int>();
+        }
+
+        private void FillList()
+        {
+            Random randomInt = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                o_instance.o_listOfInts.Add(randomInt.Next(int.MaxValue));
+            }
+            Console.WriteLine("Nowa lista zostala zainicjalizowana i wypełniona!");
+        }
+
+        public int getTheLastNumber()
+        {   
+            lock (singletonlock)
+            {
+                int tempNumber = o_listOfInts.Last();
+                o_listOfInts.RemoveAt(o_listOfInts.Count - 1);
+                if (o_listOfInts.Count == 0) isEmpty = true;
+
+                return tempNumber;
+            }            
+        }        
+    }
+}
--- ThreadsExercises_1/Program.cs
+++ ThreadsExercises_1/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ThreadsExercises_1
@@ -9,7 +10,30 @@ namespace ThreadsExercises_1
     class Program
     {
         static void Main(string[] args)
+        {            
+            Thread thr_1 = new Thread(GetElement);
+            thr_1.Name = "Watek 1";
+            Thread thr_2 = new Thread(GetElement);
+            thr_2.Name = "Watek 2";
+
+            thr_1.Start();                
+            thr_2.Start();       
+
+
+            Console.WriteLine("\nProgram zakonczyl dzialanie!");
+            Console.ReadLine();
+        }
+
+        public static void GetElement()
         {
+            while (!ContainerOfInt.Instance.isEmpty)
+            {                
+                Console.WriteLine("{0} pobrał wartość [{1}]!",
+                                    Thread.CurrentThread.Name,
+                                    ContainerOfInt.Instance.getTheLastNumber());
+                Random rndTime = new Random();
+                Thread.Sleep(rndTime.Next(10));                                 
+            }
         }
     }
 }
